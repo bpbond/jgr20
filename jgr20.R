7b6638406f9dc6db13f6a39df4b824f48fc89744
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2023-04-21 20:56:01 -0400

    Update jgr20.R

--- jgr20.R
+++ jgr20.R
@@ -1,4 +1,5 @@
 # Estimates of global Rh and Rs based on ResCOM spreadsheet
+# BBL 2023-04-21
 
 library(readr)
 library(ggplot2)
@@ -7,37 +8,40 @@ theme_set(theme_bw())
 dat <- read_csv("ResCom_ Global Respiration estimates and trends - Sheet1.csv", skip = 1)
 
 # Extract "xxxx (yyyy)" into a year column
+# First isolate the four-digit numbers in the strings; regexpr returns
+# a starting position and, as an attribute, the length of the match (always 4)
 year_locs <- regexpr("[0-9]{4}", dat$Citation)
 years <- sapply(seq_along(year_locs), function(x) {
   if(isTRUE(year_locs[x] > 0)) {
-    substr(dat$Citation[x], year_locs[x], year_locs[x] + attr(year_locs, "match.length")[x] - 1)
+    substr(dat$Citation[x], year_locs[x], year_locs[x] + 3) # 3 = 4-1
   } else {
-    NA
+    NA_character_
   }
 })
 dat$Year <- as.integer(years)
 
-# Handle flux entries that are "x to y"
+# Create easier-to-work-with Flux_mean and Flux_sd columns
 dat$Flux_mean <- as.numeric(dat$`Mean or median\n(Pg C yr-1)`)
 dat$Flux_sd <- as.numeric(dat$`Std\n(PgC yr-1)`)
 
+# Handle flux entries that are "x to y"
 have_to <- grep("to", dat$`Mean or median\n(Pg C yr-1)`)
+# For those entries, split the strings and convert each to numeric
 split_vals <- strsplit(dat$`Mean or median\n(Pg C yr-1)`[have_to], " to ")
 lows <- sapply(split_vals, function(x) as.numeric(x[1]))
 highs <- sapply(split_vals, function(x) as.numeric(x[2]))
-
-dat$Flux_mean[have_to] <- (lows + highs) / 2
-dat$Flux_sd[have_to] <- (highs - lows) / 2
+# The flux mean and sd are calculated based on the provided range
+dat$Flux_mean[have_to] <- (lows + highs) / 2 # average
+dat$Flux_sd[have_to] <- (highs - lows) / 2   # half the range 
 
 # Plots
 
 dat <- subset(dat, !is.na(Flux_mean))
 
+# Rs, total soil respiration
 rs <- subset(dat, Flux == "Total Rs")
-
 ggplot(rs, aes(Year, Flux_mean)) +  geom_linerange(aes(ymin = Flux_mean - Flux_sd, ymax = Flux_mean + Flux_sd), color= "grey") + geom_point(aes(color = `Model type`))
 
+# Rh, heterotrophic soil respiration
 rh <- subset(dat, Flux == "Belowground Rh")
-
 ggplot(rh, aes(Year, Flux_mean)) +  geom_linerange(aes(ymin = Flux_mean - Flux_sd, ymax = Flux_mean + Flux_sd), color= "grey") + geom_point(aes(color = `Model type`))
-
